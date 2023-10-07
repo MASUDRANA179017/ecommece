15cdcd204abadb8d67e177abf09e9704f3f8d5c1
Masud Rana <68780186+MASUDRANA179017@users.noreply.github.com>
2023-10-08 02:53:57 +0600

    bg tailwin css gride

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,22 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+
+    container: {
+        center: true,
+    },
+    screens: {
+      'xs': '575px',
+      'sm': '767px',
+      'md': '991px',
+      'lg': '1199px',
+      'xl': '1399px',
+      '2xl': '1600px',
+    },
+
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
       },
     },
   },
